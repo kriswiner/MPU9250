d2e0a9b70a363d5284a1e08a21c9fd283c4ae3c7
Owen L - SFE <oclyke@gmail.com>
2018-08-01 16:47:35 -0600

    Added unit tester for I2C

---
+++ examples/Example9999_UnitTest/Example9999_UnitTest.ino
@@ -0,0 +1,37 @@
+#include "MPU9250.h"
+
+#define I2Cclock 400000
+#define I2Cport Wire
+#define MPU9250_ADDRESS MPU9250_ADDRESS_AD0   // Use either this line or the next to select which I2C address your device is using
+//#define MPU9250_ADDRESS MPU9250_ADDRESS_AD1
+
+MPU9250 myIMU(MPU9250_ADDRESS, I2Cport, I2Cclock);
+
+ byte c = 0x00;
+ byte d = 0x00;
+ bool ledOn = true;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(38400);
+  while(!Serial){};
+
+  pinMode(13, OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+ c = myIMU.readByte(MPU9250_ADDRESS_AD0, WHO_AM_I_MPU9250);
+ d = myIMU.readByte(MPU9250_ADDRESS_AD1, WHO_AM_I_MPU9250);
+
+  Serial.print("Received AD0: 0x");
+  Serial.print(c, HEX);
+  Serial.print(", AD1: 0x");
+  Serial.println(d, HEX);
+  digitalWrite(13, ledOn);
+  ledOn = !ledOn;
+  delay(100);
+
+}
