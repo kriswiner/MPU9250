01bfca8cdedd9945f16f0bdb107152b95321c015
Owen L - SFE <oclyke@gmail.com>
2018-08-01 09:38:53 -0600

    Fixed addresses
    - Changed magnetometer WAI register to 0x00 as per datasheet and pull request
    - Removed preprocessor logic to set up the defined MPU9250_ADDRESS, instead allowing the user to define that value as one of the two options MPU9250_ADDRESS_AD0 or MPU9250_ADDRESS_AD1

--- src/MPU9250.h
+++ src/MPU9250.h
@@ -19,8 +19,7 @@
 // a different register map
 
 //Magnetometer Registers
-#define AK8963_ADDRESS   0x0C
-#define WHO_AM_I_AK8963  0x49 // (AKA WIA) should return 0x48
+#define WHO_AM_I_AK8963  0x00 // (AKA WIA) should return 0x48
 #define INFO             0x01
 #define AK8963_ST1       0x02  // data ready status bit 0
 #define AK8963_XOUT_L    0x03  // data
@@ -171,13 +170,12 @@
 
 // Using the MPU-9250 breakout board, ADO is set to 0
 // Seven-bit device address is 110100 for ADO = 0 and 110101 for ADO = 1
-#define ADO 0
-#if ADO
-#define MPU9250_ADDRESS 0x69  // Device address when ADO = 1
-#else
-#define MPU9250_ADDRESS 0x68  // Device address when ADO = 0
+// The previous preprocessor directives were sensitive to the location that the user defined AD1
+// Now simply define MPU9250_ADDRESS as one of the two following depending on your application
+#define MPU9250_ADDRESS_AD1 0x69  // Device address when ADO = 1
+#define MPU9250_ADDRESS_AD0 0x68  // Device address when ADO = 0
 #define AK8963_ADDRESS  0x0C   // Address of magnetometer
-#endif // AD0
+
 
 #define READ_FLAG 0x80
 #define NOT_SPI -1
